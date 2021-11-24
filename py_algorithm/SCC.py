bcb34d7f7ae963070f1972f4504958b9e8acea6e
atgane <hyper2012@naver.com>
2021-11-24 14:23:32 +0900

    update SCC

--- py_algorithm/SCC.py
+++ py_algorithm/SCC.py
@@ -1,36 +1,64 @@
 # graph(list): 간선의 정보를 담은 리스트
-# graph[][0](int): 출발 정점의 번호
-# graph[][1](int): 도착 정점의 번호
+# graph[i](list): graph의 i번째 노드가 어디로 도착할 수 있는지
 # inv_graph(list): graph와 같으나 간선의 방향이 반대
 # N(int): 노드의 개수
+# scc(list): i번째 노드가 몇 번째 SCC에 포함되는지?
+# scc_map(list): scc의 연결정보를 담은 리스트
+# scc_map[i](list): scc의 i번째 노드가 어디로 도착할 수 있는지
+# scc_num(int): scc의 개수
 
 def find_scc(graph, inv_graph, N):
-    def init_dfs(node, graph, visited, stack):
-        visited[node] = True
-        for child_node in graph[node]:
-            if not visited[child_node]:
-                init_dfs(child_node, graph, visited, stack)
-        stack.append(node)
+    def init_dfs(node):
+        inner_stack = [node]
+        while inner_stack:
+            node = inner_stack[-1]
+            visited[node] = True
+            have_child_node = False
+            for child_node in graph[node]:
+                if not visited[child_node]:
+                    inner_stack.append(child_node)
+                    have_child_node = True
+                    visited[child_node] = True
+                    break
+            if not have_child_node:
+                stack.append(inner_stack.pop())
+
 
     visited = [False for _ in range(N + 1)]
     stack = []
     for i in range(1, N + 1):
         if not visited[i]:
-            init_dfs(i, graph, visited, stack)
+            init_dfs(i)
 
-    def find_scc(node, inv_graph, local_scc, visited):
-        visited[node] = True
-        for child_node in inv_graph[node]:
-            if not visited[child_node]:
-                find_scc(child_node, inv_graph, local_scc, visited)
-        local_scc.append(node)
+    def find_scc(node, scc_num):
+        inner_stack = [node]
+        while inner_stack:
+            node = inner_stack[-1]
+            visited[node] = True
+            have_child_node = False
+            for child_node in inv_graph[node]:
+                if not visited[child_node]:
+                    inner_stack.append(child_node)
+                    have_child_node = True
+                    visited[child_node] = True
+                    break
+            if not have_child_node:
+                scc[node] = scc_num
+                inner_stack.pop()
 
-    scc = []
+    scc = [0 for _ in range(N + 1)]
+    scc_num = 1
+
     for i in range(N + 1): visited[i] = False
     for i in range(1, N + 1):
         node = stack.pop()
         if not visited[node]:
-            local_scc = []
-            find_scc(node, inv_graph, local_scc, visited)
-            scc.append(local_scc)
-    return scc
+            find_scc(node, scc_num)
+            scc_num += 1
+    scc_num -= 1
+    scc_map = [[] for _ in range(scc_num + 1)]
+    for node in range(1, N + 1):
+        for child_node in graph[node]:
+            if scc[node] != scc[child_node] and scc[child_node] not in scc_map[scc[node]]:
+                scc_map[scc[node]].append(scc[child_node])
+    return scc, scc_map, scc_num
