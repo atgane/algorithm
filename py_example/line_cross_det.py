27eda1b1bd67f0734666ec0e8bad5cfae5599477
atgane <hyper2012@naver.com>
2022-01-12 10:49:28 +0900

    update line cross algo

--- py_example/line_cross_det.py
+++ py_example/line_cross_det.py
@@ -1,26 +1,26 @@
 import sys
 ssr = sys.stdin.readline
 
-def cross_product(ux, uy, vx, vy):
-    t = ux * vy - vx * uy
-    if t > 0: return 1
-    if t < 0: return -1
-    return 0
+def cross(o, a, b):
+    return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
 
-a = []
-a.extend(list(map(int, ssr().split())))
-a.extend(list(map(int, ssr().split())))
-
-a1 = cross_product(a[0] - a[2], a[1] - a[3], a[2] - a[4], a[3] - a[5])
-a2 = cross_product(a[0] - a[2], a[1] - a[3], a[2] - a[6], a[3] - a[7])
-a3 = cross_product(a[4] - a[6], a[5] - a[7], a[6] - a[0], a[7] - a[1])
-a4 = cross_product(a[4] - a[6], a[5] - a[7], a[6] - a[2], a[7] - a[3])
-
-ans = 0
-if a1 * a2 <= 0 and a3 * a4 <= 0:
-    if a1 * a2 == 0 and a3 * a4 == 0:
-        if min(a[0], a[2]) <= max(a[4], a[6]) and min(a[1], a[3]) <= max(a[5], a[7]) and min(a[4], a[6]) <= max(a[0], a[2]) and min(a[5], a[7]) <= max(a[1], a[3]):
+def cross_check(line1, line2):
+    a1 = cross(line1[0], line1[1], line2[0])
+    a2 = cross(line1[0], line1[1], line2[1])
+    a3 = cross(line2[0], line2[1], line1[0])
+    a4 = cross(line2[0], line2[1], line1[1])
+    ans = 0
+    if a1 * a2 <= 0 and a3 * a4 <= 0:
+        if a1 * a2 == 0 and a3 * a4 == 0:
+            if min(line1[0][0], line1[1][0]) <= max(line2[0][0], line2[1][0]) and min(line1[0][1], line1[1][1]) <= max(line2[0][1], line2[1][1]) \
+                and min(line2[0][0], line2[1][0]) <= max(line1[0][0], line1[1][0]) and min(line2[0][1], line2[1][1]) <= max(line1[0][1], line1[1][1]):
+                ans = 1
+        else:
             ans = 1
-    else:
-        ans = 1
-print(ans)
+    return ans
+
+a1, a2, a3, a4 = map(int, ssr().split())
+line1 = [[a1, a2], [a3, a4]]
+b1, b2, b3, b4 = map(int, ssr().split())
+line2 = [[b1, b2], [b3, b4]]
+print(cross_check(line1, line2))
